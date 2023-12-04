4b247ff4864aac2ed6b8f0bf8133864092f0c4a1
アレクサンダー.eth <gpg@pavlovcik.com>
2023-12-04 20:24:52 +0900

    feat: animated grid

--- src/home/home.ts
+++ src/home/home.ts
@@ -1,61 +1,9 @@
+import { grid } from "../the-grid";
 import { authentication } from "./authentication";
-import { SORTING_OPTIONS, Sorting, fetchGitHubIssues } from "./fetch-github-issues";
+import { fetchGitHubIssues } from "./fetch-github-issues";
+import { sortingButtons } from "./sorting-buttons";
 
 fetchGitHubIssues().catch((error) => console.error(error));
 authentication();
-filterButtons();
-
-function filterButtons() {
-  const filters = document.getElementById("filters");
-  if (!filters) throw new Error("filters not found");
-
-  const labels = document.createElement("div");
-  labels.className = "labels";
-
-  let lastChecked: HTMLInputElement | null = null;
-
-  SORTING_OPTIONS.forEach((option) => {
-    const dfg = document.createDocumentFragment();
-    const div = document.createElement("div");
-    const label = document.createElement("label") as HTMLLabelElement;
-    const input = document.createElement("input");
-    input.type = "radio";
-    input.value = option;
-    input.id = option;
-    input.name = "sort";
-
-    const defaultIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M480-345 240-585l56-56 184 184 184-184 56 56-240 240Z"/></svg>`;
-    const activeIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M480-200 240-440l56-56 184 183 184-183 56 56-240 240Zm0-240L240-680l56-56 184 183 184-183 56 56-240 240Z"/></svg>`;
-
-    label.htmlFor = option;
-    div.className = "default icon";
-    div.innerHTML = defaultIcon;
-    const divActive = div.cloneNode(true) as HTMLDivElement;
-    divActive.className = "active icon";
-    divActive.innerHTML = activeIcon;
-    label.appendChild(div);
-    label.appendChild(divActive);
-    label.appendChild(document.createTextNode(option.charAt(0).toUpperCase() + option.slice(1)));
-    dfg.appendChild(input);
-    dfg.appendChild(label);
-    labels.appendChild(dfg);
-
-    input.addEventListener("click", () => {
-      if (input === lastChecked) {
-        input.checked = false;
-        lastChecked = null;
-        fetchGitHubIssues().catch((error) => console.error(error));
-      } else {
-        lastChecked = input;
-        fetchGitHubIssues(input.value as Sorting).catch((error) => console.error(error));
-      }
-    });
-  });
-  filters.appendChild(labels);
-
-  // const sortLabels = filters.querySelectorAll("input");
-
-  // sortLabels.forEach((label) => {
-
-  // });
-}
+sortingButtons();
+grid(document.getElementById("grid") as HTMLElement);
