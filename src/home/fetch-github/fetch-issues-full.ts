aa4994828fd0b92a645f5078036e14b352945b6b
アレクサンダー.eth <gpg@pavlovcik.com>
2024-01-24 16:59:32 +0900

    fix: avatar image blobs save in indexed db and load from there as a cache

--- src/home/fetch-github/fetch-issues-full.ts
+++ src/home/fetch-github/fetch-issues-full.ts
@@ -1,5 +1,6 @@
 import { Octokit } from "@octokit/rest";
 import { getGitHubAccessToken } from "../getters/get-github-access-token";
+import { saveImageToDB } from "../getters/get-indexed-db";
 import { getLocalStore } from "../getters/get-local-store";
 import { GitHubIssue } from "../github-types";
 import { PreviewToFullMapping } from "./preview-to-full-mapping";
@@ -50,18 +51,22 @@ export function fetchIssuesFull(previews: GitHubIssue[]) {
           } else {
             organizationImageCache.push({ [orgName]: null });
           }
-
           return octokit.rest.orgs.get({ org: orgName }).then(({ data }) => {
             const avatarUrl = data.avatar_url;
-            // Store the avatar URL in the cache
-            localStorage.setItem(`avatarUrl-${orgName}`, avatarUrl);
-            // Return a promise that resolves when the avatar image has been fetched
-            return new Promise((resolve) => {
-              const image = new Image();
-              image.onload = () => resolve(full);
-              image.onerror = () => resolve(full);
-              image.src = avatarUrl;
-            });
+            // Fetch the image as a Blob and save it to IndexedDB
+            return fetch(avatarUrl)
+              .then((response) => response.blob())
+              .then(async (blob) => {
+                await saveImageToDB({
+                  dbName: "ImageDatabase",
+                  storeName: "ImageStore",
+                  keyName: "name",
+                  orgName: `avatarUrl-${orgName}`,
+                  avatarBlob: blob,
+                });
+                organizationImageCache.push({ [orgName]: blob });
+                return full;
+              });
           });
         }
         return full;
