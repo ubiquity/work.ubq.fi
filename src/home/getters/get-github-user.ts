5b66bd43a5b6472f0a06dbad8946c3dbba09255b
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-03-22 14:45:35 +0000

    fix: remove err and code check

--- src/home/getters/get-github-user.ts
+++ src/home/getters/get-github-user.ts
@@ -27,20 +27,15 @@ async function getNewSessionToken(): Promise<string | null> {
   const params = new URLSearchParams(hash.substr(1)); // remove the '#' and parse
   const providerToken = params.get("provider_token");
   if (!providerToken) {
-    const err = params.get("error");
-    const code = params.get("error_code");
     const desc = params.get("error_description");
 
-    if (err === "server_error") {
-      if (code === "500") {
-        if (desc === "Error getting user profile from external provider") {
-          // without a token we can't get a dynamic retry timeframe
-          displayPopupMessage(`GitHub Login Provider`, `Your access token may have reached it's rate limit, please try again after one hour.`);
-          console.error("GitHub login provider");
-        }
-      }
+    if (desc === "Error getting user profile from external provider") {
+      // without a token we can't get a dynamic retry timeframe
+      displayPopupMessage(`GitHub Login Provider`, `Your access token may have reached it's rate limit, please try again after one hour.`);
+      console.error("GitHub login provider");
     }
   }
+
   return providerToken || null;
 }
 
