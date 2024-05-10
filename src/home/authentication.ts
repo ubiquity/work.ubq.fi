4f621090f0eab950e90f9194a9e3e59a890130b0
gentlementlegen <fernand.veyrier@epitech.eu>
2024-05-10 17:17:39 +0900

    feat: unlock scopes button is displayed when scopes are missing

--- src/home/authentication.ts
+++ src/home/authentication.ts
@@ -1,14 +1,30 @@
+import { Octokit } from "@octokit/rest";
 import { trackDevRelReferral } from "./devrel-tracker";
 import { getGitHubAccessToken } from "./getters/get-github-access-token";
 import { getGitHubUser } from "./getters/get-github-user";
 import { GitHubUser } from "./github-types";
 import { displayGitHubUserInformation } from "./rendering/display-github-user-information";
-import { renderGitHubLoginButton } from "./rendering/render-github-login-button";
+import { renderAugmentAccessButton, renderGitHubLoginButton } from "./rendering/render-github-login-button";
+
+/**
+ * Checks if the logged-in user is part of Ubiquity's Org, and didn't grant the 'repo' scope
+ */
+async function isOrgMemberWithoutScope(accessToken: string) {
+  const octokit = new Octokit({ auth: accessToken });
+  const { headers } = await octokit.request("HEAD /");
+  if (headers) {
+    const scopes = headers["x-oauth-scopes"]?.split(", ");
+    return !scopes?.includes("repo");
+  }
+  return false;
+}
 
 export async function authentication() {
   const accessToken = await getGitHubAccessToken();
   if (!accessToken) {
     renderGitHubLoginButton();
+  } else if (await isOrgMemberWithoutScope(accessToken)) {
+    renderAugmentAccessButton();
   }
 
   const gitHubUser: null | GitHubUser = await getGitHubUser();
--- src/home/rendering/render-github-login-button.ts
+++ src/home/rendering/render-github-login-button.ts
@@ -1,6 +1,4 @@
-import { Octokit } from "@octokit/rest";
 import { createClient } from "@supabase/supabase-js";
-import { getGitHubAccessToken } from "../getters/get-github-access-token";
 import { toolbar } from "../ready-toolbar";
 
 declare const SUPABASE_URL: string; // @DEV: passed in at build time check build/esbuild-build.ts
@@ -20,19 +18,7 @@ export async function checkSupabaseSession() {
   return session;
 }
 
-async function gitHubLoginButtonHandler() {
-  const scopes = "public_repo";
-  // TODO check user member?
-  try {
-    console.log("1. gitHubLoginButtonHandler");
-    const octokit = new Octokit({ auth: await getGitHubAccessToken() });
-    console.log("2. gitHubLoginButtonHandler");
-    const data = await octokit.rest.orgs.get();
-    console.log("3. gitHubLoginButtonHandler");
-    console.log(data);
-  } catch (e) {
-    console.error(e);
-  }
+async function gitHubLoginButtonHandler(scopes = "public_repo read:org") {
   const { error } = await supabase.auth.signInWithOAuth({
     provider: "github",
     options: {
@@ -43,14 +29,27 @@ async function gitHubLoginButtonHandler() {
     console.error("Error logging in:", error);
   }
 }
+
+const augmentAccessButton = document.createElement("button");
+export function renderAugmentAccessButton() {
+  augmentAccessButton.id = "augment-access-button";
+  augmentAccessButton.innerHTML = `<span title="Allow access to private repositories"><svg viewBox="0 0 24 24" class="svg-icon"><path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2m-6 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2m3.1-9H8.9V6c0-1.71 1.39-3.1 3.1-3.1 1.71 0 3.1 1.39 3.1 3.1z"></path></svg><span/>`;
+  augmentAccessButton.addEventListener("click", () => gitHubLoginButtonHandler("repo read:org"));
+  if (toolbar) {
+    toolbar.appendChild(augmentAccessButton);
+    toolbar.classList.add("ready");
+  }
+}
+
 const gitHubLoginButton = document.createElement("button");
 export function renderGitHubLoginButton() {
   gitHubLoginButton.id = "github-login-button";
   gitHubLoginButton.innerHTML = "<span>Login</span><span class='full'>&nbsp;With GitHub</span>";
-  gitHubLoginButton.addEventListener("click", gitHubLoginButtonHandler);
+  gitHubLoginButton.addEventListener("click", () => gitHubLoginButtonHandler());
   if (toolbar) {
     toolbar.appendChild(gitHubLoginButton);
     toolbar.classList.add("ready");
   }
 }
+
 export { gitHubLoginButton };
