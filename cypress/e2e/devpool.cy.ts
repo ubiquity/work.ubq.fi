c3d83c825de1090e84b43470a0793ef6f707a2c7
ARREY-ETTA BESSONG EKEP OBASI <arreyettaekep@gmail.com>
2024-07-26 14:34:33 +0100

    chore: removed hardcoded values in test

--- cypress/e2e/devpool.cy.ts
+++ cypress/e2e/devpool.cy.ts
@@ -205,6 +205,49 @@ describe("DevPool", () => {
     cy.get('div[id="issues-container"]').children().should("have.length", 2);
   });
 
+  it("Should save referral code to Supabase referral table upon login", () => {
+    const referralCode = "test-referral-code";
+
+    cy.intercept("https://api.github.com/user**", (req) => {
+      req.reply({
+        statusCode: 200,
+        body: githubUser,
+      });
+    }).as("getUser");
+
+    cy.intercept("POST", `${Cypress.env("SUPABASE_URL")}/rest/v1/referrals`, (req) => {
+      expect(req.body).to.have.property("referral_code", referralCode);
+      expect(req.body).to.have.property("dev_github", `${githubUser.login}|${githubUser.id}`);
+      req.reply({
+        statusCode: 201,
+      });
+    }).as("saveReferral");
+
+    const loginUrl = `?ref=${referralCode}`;
+    cy.visit(loginUrl);
+    cy.get("#github-login-button").click();
+
+    cy.intercept("https://github.com/login**", (req) => {
+      req.reply({
+        statusCode: 200,
+      });
+
+      window.localStorage.setItem(
+        `sb-${Cypress.env("SUPABASE_STORAGE_KEY")}-auth-token`,
+        JSON.stringify({
+          provider_token: "token",
+          access_token: "token",
+          token_type: "bearer",
+          user: githubUser,
+        })
+      );
+    }).as("githubLogin");
+
+    cy.visit("/");
+
+    cy.wait("@saveReferral");
+  });
+
   it("User can log in", () => {
     cy.intercept("https://api.github.com/user**", (req) => {
       req.reply({
@@ -247,49 +290,6 @@ describe("DevPool", () => {
     cy.visit("/");
     cy.get("#authenticated").should("exist");
     cy.get("#filter-top").should("be.visible");
-  });
-
-  it("Should save referral code to Supabase referral table upon login", () => {
-    const referralCode = "test-referral-code";
-
-    cy.intercept("https://api.github.com/user**", (req) => {
-      req.reply({
-        statusCode: 200,
-        body: githubUser,
-      });
-    }).as("getUser");
-
-    cy.intercept("POST", "https://lelwqeowubwpkhjlzsiu.supabase.co/rest/v1/referrals", (req) => {
-      expect(req.body).to.have.property("referral_code", referralCode);
-      expect(req.body).to.have.property("dev_github", `${githubUser.login}|${githubUser.id}`);
-      req.reply({
-        statusCode: 201
-      });
-    }).as("saveReferral");
-
-    const loginUrl = `?ref=${referralCode}`;
-    cy.visit(loginUrl);
-    cy.get("#github-login-button").click();
-
-    cy.intercept("https://github.com/login**", (req) => {
-      req.reply({
-        statusCode: 200,
-      });
-
-      window.localStorage.setItem(
-        `sb-${Cypress.env("SUPABASE_STORAGE_KEY")}-auth-token`,
-        JSON.stringify({
-          provider_token: "token",
-          access_token: "token",
-          token_type: "bearer",
-          user: githubUser,
-        })
-      );
-    }).as("githubLogin");
-
-    cy.visit("/");
-
-    cy.wait("@saveReferral");
   });
 
   describe("Display error modal", () => {
