334442e26383130dbc6b32a64a34dfcd72b6deb4
xno-miner <xnominer@gmail.com>
2024-08-20 23:31:45 +0200

    fix: don't show price sorting button when RFC is true

--- src/home/sorting/generate-sorting-buttons.ts
+++ src/home/sorting/generate-sorting-buttons.ts
@@ -1,6 +1,7 @@
+declare const IS_RFC: boolean;
 import { SortingManager } from "./sorting-manager";
 
-export const SORTING_OPTIONS = ["price", "time", "priority", "activity"] as const;
+export const SORTING_OPTIONS = IS_RFC ? ["time", "priority", "activity"] : ["price", "time", "priority", "activity"] as const;
 export type Sorting = (typeof SORTING_OPTIONS)[number];
 
 export function generateSortingToolbar() {
