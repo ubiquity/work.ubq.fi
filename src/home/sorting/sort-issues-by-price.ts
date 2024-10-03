b88b5d1b235b69269ba62ee9d163cb96b68ee113
アレクサンダー.eth <gpg@pavlovcik.com>
2024-10-04 07:10:48 +0900

    fix: type error

--- src/home/sorting/sort-issues-by-price.ts
+++ src/home/sorting/sort-issues-by-price.ts
@@ -1,18 +1,19 @@
-// @ts-nocheck
-
 import { GitHubIssue } from "../github-types";
 
 export function sortIssuesByPrice(issues: GitHubIssue[]) {
   return issues.sort((a, b) => {
-    const aPriceLabel = a.labels.find((label) => label.name.startsWith("Price: "));
-    const bPriceLabel = b.labels.find((label) => label.name.startsWith("Price: "));
-
-    const aPriceMatch = aPriceLabel ? aPriceLabel.name.match(/Price: (\d+)/) : null;
-    const bPriceMatch = bPriceLabel ? bPriceLabel.name.match(/Price: (\d+)/) : null;
-
-    const aPrice = aPriceMatch && aPriceMatch[1] ? parseInt(aPriceMatch[1], 10) : 0;
-    const bPrice = bPriceMatch && bPriceMatch[1] ? parseInt(bPriceMatch[1], 10) : 0;
+    const aPrice = a.labels.map(getPriceFromLabel).find((price) => price !== null) ?? -1;
+    const bPrice = b.labels.map(getPriceFromLabel).find((price) => price !== null) ?? -1;
 
     return bPrice - aPrice;
   });
+}
+
+function getPriceFromLabel(label: string | { name?: string }) {
+  if (typeof label === "string" || !label.name) return null;
+  if (label.name.startsWith("Price: ")) {
+    const match = label.name.match(/Price: (\d+)/);
+    return match ? parseInt(match[1], 10) : null;
+  }
+  return null;
 }
