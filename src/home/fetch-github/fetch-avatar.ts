605a82990b862acb5305703d4dfd56494f79f2e6
zugdev <zug.dev@proton.me>
2024-10-08 20:58:10 -0300

    chore: remove fetch-avatar console logs

--- src/home/fetch-github/fetch-avatar.ts
+++ src/home/fetch-github/fetch-avatar.ts
@@ -11,25 +11,20 @@ export async function fetchAvatar(orgName: string): Promise<Blob | void> {
   // Check if the avatar is already cached in memory
   const cachedAvatar = organizationImageCache.get(orgName);
   if (cachedAvatar) {
-    console.log(`Returning cached avatar for ${orgName}`);
     return cachedAvatar;
   }
 
   // If there's a pending fetch for this organization, wait for it to complete
   if (pendingFetches.has(orgName)) {
-    console.log(`Waiting for ongoing fetch for ${orgName}`);
     return pendingFetches.get(orgName);
   }
 
   // Start the fetch process and store the promise in the pending fetches map
   // It will try to fetch from IndexedDB first, then from GitHub organizations, and finally from GitHub users, returning in the first successful step
   const fetchPromise = (async () => {
-    console.log(`Attempting to fetch avatar for ${orgName}`);
-
     // Step 1: Try to get the avatar from IndexedDB
     const avatarBlob = await getImageFromCache({ dbName: "GitHubAvatars", storeName: "ImageStore", orgName: `avatarUrl-${orgName}` });
     if (avatarBlob) {
-      console.log(`Avatar found in IndexedDB for ${orgName}`);
       organizationImageCache.set(orgName, avatarBlob); // Cache it in memory
       return avatarBlob;
     }
@@ -43,7 +38,6 @@ export async function fetchAvatar(orgName: string): Promise<Blob | void> {
       } = await octokit.rest.orgs.get({ org: orgName });
 
       if (avatarUrl) {
-        console.log(`Fetching avatar from network for ${orgName}`);
         const response = await fetch(avatarUrl);
         const blob = await response.blob();
 
@@ -57,7 +51,6 @@ export async function fetchAvatar(orgName: string): Promise<Blob | void> {
         });
 
         organizationImageCache.set(orgName, blob);
-        console.log(`Avatar cached for ${orgName}`);
         return blob;
       }
     } catch (orgError) {
@@ -71,7 +64,6 @@ export async function fetchAvatar(orgName: string): Promise<Blob | void> {
       } = await octokit.rest.users.getByUsername({ username: orgName });
 
       if (avatarUrl) {
-        console.log("Fetching avatar from network by user", orgName);
         const response = await fetch(avatarUrl);
         const blob = await response.blob();
 
@@ -85,7 +77,6 @@ export async function fetchAvatar(orgName: string): Promise<Blob | void> {
         });
 
         organizationImageCache.set(orgName, blob);
-        console.log(`Avatar cached for user ${orgName}`);
         return blob;
       }
     } catch (innerError) {
