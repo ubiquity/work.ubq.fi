26c9e76b70c9a925c7e61b1c3861f300a565a17f
zugdev <zug.dev@proton.me>
2024-10-08 21:24:16 -0300

    feat: move rateLimitModal to handle-rate-limit.ts

--- src/home/fetch-github/fetch-issues-preview.ts
+++
@@ -1,6 +0,0 @@
-// this file could be renamed or straight up deleted content could be moved to handle rate limit
-import { displayPopupMessage } from "../rendering/display-popup-modal";
-
-export function rateLimitModal(message: string) {
-  displayPopupMessage({ modalHeader: `GitHub API rate limit exceeded.`, modalBody: message, isError: false });
-}
