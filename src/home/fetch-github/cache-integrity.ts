b28d01c8e37864881899324bbc5f7165244abd5a
zugdev <zug.dev@proton.me>
2024-10-08 21:06:49 -0300

    feat: add a cache integrity helper

---
+++ src/home/fetch-github/cache-integrity.ts
@@ -0,0 +1,14 @@
+import { getLocalStore } from "../getters/get-local-store";
+import { GITHUB_TASKS_STORAGE_KEY, TaskStorageItems } from "../github-types";
+import { taskManager } from "../home";
+
+export async function checkCacheIntegrityAndSyncTasks() {
+    console.log("Checking cache integrity");
+    const _cachedTasks = getLocalStore(GITHUB_TASKS_STORAGE_KEY) as TaskStorageItems;
+    
+    // if there are no cached tasks, or timestamp is invalid, or tasks were cached over 15 minutes ago resync tasks
+    if (!_cachedTasks || !_cachedTasks.timestamp || _cachedTasks.timestamp + 60 * 1000 * 15 <= Date.now()) {
+        await taskManager.syncTasks();
+    }
+}
+  
