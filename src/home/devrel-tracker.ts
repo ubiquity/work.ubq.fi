134db4d3ba513c6c4ba62817b16abbc5a0e90009
koya0 <namikoyaobi@proton.me>
2024-10-12 17:49:24 -0300

    feat: write new referral tracker

--- src/home/devrel-tracker.ts
+++ src/home/devrel-tracker.ts
@@ -1,22 +1,33 @@
+import fetch from 'node-fetch';
+
+declare const WORKER_URL: string; // @DEV: passed in at build time check build/esbuild-build.ts
+
 export function initiateDevRelTracking() {
   const oldDevRelCode = localStorage.getItem("devRel");
   if (!oldDevRelCode) {
     const urlParams = new URLSearchParams(window.location.search);
-    const devRelCode = urlParams.get("devRel");
+    const devRelCode = urlParams.get("ref");
     if (devRelCode) {
       localStorage.setItem("devRel", devRelCode);
     }
   }
 }
 
-export function trackDevRelReferral(devGithub: string) {
+export async function trackDevRelReferral(devGitHubId: number) {
   const devRelCode = localStorage.getItem("devRel");
+
+  // key: user_id (devGitHubId), value: referral_id (devRelCode)
   if (devRelCode && devRelCode != "done") {
-    // @ts-expect-error : using global gtag
-    gtag("event", "ethSeoul_registration", {
-      devRel: devRelCode,
-      devGithub: devGithub,
+    const url = `${WORKER_URL}/set?key=${encodeURIComponent(devGitHubId)}&value=${encodeURIComponent(devRelCode)}`;
+
+    const response = await fetch(url, {
+        method: 'POST',
     });
-    localStorage.setItem("devRel", "done");
+
+    if (response.status === 200) {
+      localStorage.setItem("devRel", "done");
+    } else {
+      console.error(`Failed to set referral. Status: ${response.status}`);
+    }
   }
 }
