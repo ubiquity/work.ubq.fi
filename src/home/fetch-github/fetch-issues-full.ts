55889a71321f0974355af4e200751915fd975ce8
koya0 <namikoyaobi@proton.me>
2024-10-19 19:22:47 -0300

    feat: change Github issues json

--- src/home/fetch-github/fetch-issues-full.ts
+++ src/home/fetch-github/fetch-issues-full.ts
@@ -1,11 +1,11 @@
 import { GitHubIssue } from "../github-types";
 export const organizationImageCache = new Map<string, Blob | null>(); // this should be declared in image related script
 
-// Fetches the issues from `devpool-issues.json` file in the `development` branch of the `devpool-directory` repo
-// https://github.com/ubiquity/devpool-directory/blob/development/devpool-issues.json
+// Fetches the issues from devpool-issues.json file in the __STORAGE__ branch of the devpool-directory repo
+// https://github.com/ubiquity/devpool-directory/blob/__STORAGE__/devpool-issues.json
 
 export async function fetchIssues(): Promise<GitHubIssue[]> {
-  const response = await fetch("https://raw.githubusercontent.com/ubiquity/devpool-directory/refs/heads/development/devpool-issues.json");
+  const response = await fetch("https://raw.githubusercontent.com/ubiquity/devpool-directory/__STORAGE__/devpool-issues.json");
   const jsonData = await response.json();
   return jsonData;
 }
