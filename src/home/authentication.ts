f2b5ae98bb9eff6bdab7a8bda738af796414328f
zugdev <zug.dev@proton.me>
2024-10-21 03:06:14 -0300

    chore: lint

--- src/home/authentication.ts
+++ src/home/authentication.ts
@@ -10,7 +10,7 @@ export async function authentication() {
     console.warn("App is offline. Skipping authentication.");
     return;
   }
-  
+
   const accessToken = await getGitHubAccessToken();
   if (!accessToken) {
     renderGitHubLoginButton();
