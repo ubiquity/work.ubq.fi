32cc56eccd25a3949f2341c9f9e4ad3f5b708542
zugdev <zug.dev@proton.me>
2024-10-23 10:49:35 -0300

    feat: add a skip animation option to render issues

--- src/home/fetch-github/fetch-issues-full.ts
+++ src/home/fetch-github/fetch-issues-full.ts
@@ -20,9 +20,9 @@ export async function postLoadUpdateIssues() {
     const fetchedIssues = await fetchIssues();
 
     if (issuesAreDifferent(cachedIssues, fetchedIssues)) {
-      await saveIssuesToCache(cachedIssues, fetchedIssues);
+      await saveIssuesToCache(cachedIssues, fetchedIssues); // this handles stale and new issues
       await taskManager.syncTasks();
-      void displayGitHubIssues();
+      void displayGitHubIssues(undefined,undefined,true);
     }
   } catch (error) {
     console.error("Error updating issues cache", error);
