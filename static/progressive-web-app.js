9a445c6135125afd4afafd1a221c455b4e6527ae
zugdev <zug.dev@proton.me>
2024-10-24 01:04:28 -0300

    feat: bump cache version

--- static/progressive-web-app.js
+++ static/progressive-web-app.js
@@ -1,4 +1,4 @@
-const cacheName = "pwacache-v2"; // Increment this when files change
+const cacheName = "pwacache-v3"; // Increment this when files change
 const urlsToCache = [
   "/",
   "/index.html",
