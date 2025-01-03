7766a20825f47480ca9c859b1a66902d6f9e8192
Shivaditya Shivganesh <sshivaditya@gmail.com>
2025-01-03 17:34:12 -0500

    fix: update the deploy.yml

--- functions/issue-scraper.ts
+++ functions/issue-scraper.ts
@@ -1,46 +1,43 @@
 import { Context } from "./types";
 
 export const corsHeaders = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET",
-    "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET",
+  "Access-Control-Allow-Headers": "Content-Type",
 };
 
 export async function onRequest(ctx: Context): Promise<Response> {
-    const { request, env } = ctx;
-    console.log("Request received:", request.method, request.url);
-    console.log("Environment", env);
-    const url = new URL(request.url);
+  const { request, env } = ctx;
+  console.log("Request received:", request.method, request.url);
+  console.log("Environment", env);
+  const url = new URL(request.url);
 
-    try {
-        switch (request.method) {
-
-            case "GET":
-                if (url.searchParams.has("key")) {
-                    const key = url.searchParams.get("key") as string;
-                    return new Response("GET request with key: " + key + JSON.stringify(env), {
-                        headers: corsHeaders,
-                        status: 200,
-                    });
-                }
-                return new Response("GET request", {
-                    headers: corsHeaders,
-                    status: 200,
-                });
-
-            default:
-                return new Response("Method Not Allowed", {
-                    headers: corsHeaders,
-                    status: 405,
-                });
-        }
-    } catch (error) {
-        console.error("Error processing request:", error);
-        return new Response("Internal Server Error", {
+  try {
+    switch (request.method) {
+      case "GET":
+        if (url.searchParams.has("key")) {
+          const key = url.searchParams.get("key") as string;
+          return new Response("GET request with key: " + key + JSON.stringify(env), {
             headers: corsHeaders,
-            status: 500,
+            status: 200,
+          });
+        }
+        return new Response("GET request", {
+          headers: corsHeaders,
+          status: 200,
+        });
+
+      default:
+        return new Response("Method Not Allowed", {
+          headers: corsHeaders,
+          status: 405,
         });
     }
-
-
+  } catch (error) {
+    console.error("Error processing request:", error);
+    return new Response("Internal Server Error", {
+      headers: corsHeaders,
+      status: 500,
+    });
+  }
 }
