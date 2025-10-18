748eb558e605591be6cca35a7c79b055b61a2670
0x4007 <gpg@pavlovcik.com>
2025-10-18 18:45:58 +0900

    chore(commitlint): inline conventional rules to remove external extends

---
+++ commitlint.config.mjs
@@ -0,0 +1,4 @@
+// Commitlint config for Deno-driven CLI
+// Import the conventional preset via Deno's npm: spec
+import configConventional from "npm:@commitlint/config-conventional";
+export default configConventional;
